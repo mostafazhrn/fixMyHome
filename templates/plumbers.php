0a87899fbae567e07222912fcc8b179ae86e937a
Mostafazhrn <Mostafazhrn@gmail.com>
2024-08-18 21:40:21 +0300

    push styling for plumbers_php

--- templates/plumbers.php
+++ templates/plumbers.php
@@ -9,6 +9,61 @@ CTYPE html>
     <link rel="icon" href="../static/images/fixMyHome1.png" />
     
 <title>Plumbers</title>
+<style>
+* {
+*   box-sizing: border-box;
+*   }
+*
+*/* Create two equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 50%;
+  padding: 10px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+/* Style the buttons */
+.btn {
+  border: none;
+  outline: none;
+  padding: 12px 16px;
+  background-color: #f1f1f1;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background-color: #ddd;
+}
+
+.btn.active {
+  background-color: #666;
+  color: white;
+}
+footer {
+    position: fixed;
+    background: hsl(180, 1%, 62%);
+    height: 50px;
+    width: 100%;
+    bottom: 0;
+    border-top: 1px solid #CCCCCC;
+}
+footer p {
+    position: absolute;
+    text-align: center;
+    font-size: 120%;
+    font-weight: bolder;
+    top: 10%;
+    bottom: 30%;
+    right: 0;
+    left: 0;
+}
+
+</style>
 </head>
 <body>
 
