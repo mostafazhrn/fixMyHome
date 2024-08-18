9152f9351d4065cee06e6a2242d7a531ba06ec87
Mostafazhrn <Mostafazhrn@gmail.com>
2024-08-18 23:37:23 +0300

    push booking with calendly api final

---
+++ templates/bookingVisits.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../static/styles/3-header.css">
+    <link rel="stylesheet" type="text/css" href="../static/styles/3-footer.css">
+    <link rel="icon" href="../static/images/fixMyHome1.png" />
+        <title>Fix my home</title>
+  </head>
+  <body>
+    <header style="background-color:rgb(217, 217, 217);">
+      <a href="../index.php"><img src="../static/images/fixMyHome1.png" /> </a>
+      <div class="logo"></div>
+    </header>
+        <img src="../static/images/vecteezyKlyaksun.jpg" class = "center" style="width:1350px; height:200px;
+                                        image-rendering: -moz-crisp-edges;
+                                        -ms-interpolation-mode: bicubic;"></img>
+
+    <div class="container">
+      <style>
+        body {
+          background-color: rgb(243, 242, 240);
+        }
+        
+        h1 {
+          font-size: 19px;
+          margin-left: 40px;
+        }
+        </style>
+        
+        <h2 style="background-color:#d9d9d9;">Fix anything in your home with a press of a button</h2>
+        <h2 style="background-color:#d9d9d9;"> Book your visit now</h2>
+        
+         <!-- Calendly inline widget begin -->
+<div class="calendly-inline-widget" data-url="https://calendly.com/zackyerzonter" style="min-width:320px;height:700px;"></div>
+<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+<!-- Calendly inline widget end -->
+    </div>
+            <footer>
+        <p>Fix my home&#169</p>
+      </footer>
+    </body>
+  </html>
+  
