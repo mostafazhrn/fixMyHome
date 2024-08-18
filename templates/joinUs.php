71fcd74937367bc80a8759763671c7ccd07f62fc
Mostafazhrn <Mostafazhrn@gmail.com>
2024-08-18 23:38:01 +0300

    push join us page

---
+++ templates/joinUs.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../static/styles/3-header.css">
+    <link rel="stylesheet" type="text/css" href="../static/styles/3-footer.css">
+    <link rel="icon" href="../static/images/fixMyHome1.png" />
+        <title>Fix my home</title>
+  </head>
+  <body>
+    <header style="background-color:rgb(217, 217, 217);">
+      <a href="../index.php"><img src="../static/images/fixMyHome1.png" /> </a>
+      <div class="logo"></div>
+    </header>
+        <img src="../static/images/vecteezyKlyaksun.jpg" class = "center" style="width:1350px; height:200px;
+                                        image-rendering: -moz-crisp-edges;
+                                        -ms-interpolation-mode: bicubic;"></img>
+
+    <div class="container">
+      <style>
+        body {
+          background-color: rgb(243, 242, 240);
+          margin-bottom: 100px;
+        }
+        
+        h1 {
+          font-size: 19px;
+          margin-left: 40px;
+        }
+
+        p {
+          font-size: 19px;
+          margin-left: 40px;
+        }
+        </style>
+        
+        <h2 style="background-color:#d9d9d9;">Fix anything in your home with a press of a button</h2>
+        <h2 style="background-color:#d9d9d9;"> Join our team of professional technicians </h2>
+        <p> Are you a professional technician? Join us and start receiving requests from customers. </p>
+        <p> We are always looking for new technicians to join our team. If you are a professional technician and you want to join us, please send us your resume and a recent background check on our email: </p> 
+        <p>info@fixmyhome.com </p>
+        <p>and we will contact you as soon as possible. registration on our website takes from 2 to 3 working days</p>
+        <p> We are looking for technicians in the following fields: </p>
+        <ul>
+          <li> Electricians </li>
+          <li> Plumbers </li>
+          <li> Painters </li>
+          <li> TV and cable technicians </li>
+          <li> housekeeping </li>
+          <li> carpenters </li>
+            </ul>
+    </body>
+    <footer>
+        <p>Fix my home&#169</p>
+      </footer>
+  </html>
+  
