24e02df8b10611560f82b73d8f98413d09f312b8
Mostafazhrn <Mostafazhrn@gmail.com>
2024-08-18 21:37:24 +0300

    push plumbers_php head and body

---
+++ templates/plumbers.php
@@ -0,0 +1,41 @@
+CTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../static/styles/3-header.css">
+    <link rel="stylesheet" type="text/css" href="../static/styles/3-footer.css">
+    <link rel="icon" href="../static/images/fixMyHome1.png" />
+    
+<title>Plumbers</title>
+</head>
+<body>
+
+<h2 style="background-color:#d9d9d9;">Fix anything in your home with a press of a button</h2>
+<h2 style="background-color:#d9d9d9;"> Choose one of our trusted plumbers and book your visit on the spot</h2>
+
+
+<br>
+
+
+
+
+
+<div class="container">
+      <section class="filters">
+                <div class="loco">
+                    </div>
+                    <div class="loco">
+                          </div>
+                    </div>
+        </section>
+          </div>
+    
+</body>
+<footer>
+  <p>Fix my home&#169</p>
+</footer>
+</html>
+
+
